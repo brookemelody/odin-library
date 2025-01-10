9263de28515c0541679e4e75e1902e066b4ab018
Brooke <brookewu2023@gmail.com>
2025-01-09 20:47:29 -0500

    Rename numPages and haveRead parameters to pages and read

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const myLibrary = [];
  * Constructor for the Book object
  * @param {String} title the title of the book
  * @param {String} author the author of the book
- * @param {int} numPages the number of pages in the book
- * @param {boolean} haveRead whether you have read the book or not
+ * @param {int} pages the number of pages in the book
+ * @param {boolean} read whether you have read the book or not
  */
-function Book(title, author, numPages, haveRead) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.numPages = numPages;
-    this.haveRead = haveRead;
+    this.numPages = pages;
+    this.haveRead = read;
 
     // Function that can report the book info
     this.info = function() {
-        let haveReadString = haveRead ? "read" : "not read yet";
-        return title + " by " + author + ", " + numPages + " pages, " + haveReadString;
+        let haveReadString = read ? "read" : "not read yet";
+        return title + " by " + author + ", " + pages + " pages, " + haveReadString;
     }
 }
 
@@ -25,12 +25,12 @@ function Book(title, author, numPages, haveRead) {
  * Create an instance of a Book object and store the new Book into an array
  * @param {String} title 
  * @param {String} author 
- * @param {int} numPages 
- * @param {boolean} haveRead 
+ * @param {int} pages 
+ * @param {boolean} read 
  */
-function addBookToLibrary(title, author, numPages, haveRead) {
+function addBookToLibrary(title, author, pages, read) {
     // Construct a Book object using the arguments
-    let myBook = new Book(title, author, numPages, haveRead);
+    let myBook = new Book(title, author, pages, read);
 
     // Add myBook to the myLibrary array
     myLibrary.push(myBook);
@@ -43,6 +43,9 @@ addBookToLibrary('No Longer Human', 'Osamu Dazai', 176, true);
 // Target the library container div in the HTML
 const libraryContainer = document.querySelector(".library-container");
 
+/**
+ * 
+ */
 function displayAllBooks()
 {
     // Loop through the array
